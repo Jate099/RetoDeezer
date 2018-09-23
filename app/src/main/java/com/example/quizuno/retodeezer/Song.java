04b775121ee7f15e4ac67c84790a527b6ca33f5a
Jate099 <42478567+Jate099@users.noreply.github.com>
2018-09-23 00:45:04 -0500

    agrega las actividades que faltaban

--- app/src/main/java/com/example/quizuno/retodeezer/Song.java
+++ app/src/main/java/com/example/quizuno/retodeezer/Song.java
@@ -1,6 +1,27 @@
 package com.example.quizuno.retodeezer;
 
-public class Song {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class Song extends AppCompatActivity {
+
+    TextView tv_nombre;
+    TextView tv_artista;
+    TextView tv_album;
+    TextView tv_duracion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.song);
+
+        tv_nombre = findViewById(R.id.tv_nombre);
+        tv_artista = findViewById(R.id.tv_artista);
+        tv_album = findViewById(R.id.tv_album);
+        tv_duracion = findViewById(R.id.tv_duracion);
 
 
+    }
 }
